aaa55491e5345334a846005ae4b46a7406247982
Dhanush Amileneni <amilenenidhanush@gmail.com>
2024-12-15 09:53:01 -0500

    final commit

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `http://localhost:5011/api/shop/search/${keyword}`
+      `https://ecommercewebsite-backend-mubu.onrender.com/api/shop/search/${keyword}`
     );
 
     return response.data;
